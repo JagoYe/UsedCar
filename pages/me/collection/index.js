fee6dc8a52de56d0c31a4677583048dfd7616ed6
Joeyang <1665074044@qq.com>
2017-10-28 13:47:39 +0800

    Joeyang modified->js

--- pages/me/collection/index.js
+++ pages/me/collection/index.js
@@ -7,13 +7,39 @@ Page({
    */
   data: {
     condition: '0',
-    bgColor: 'bgColor'
+    bgColor: 'bgColor',
+    display: false
   },
   tabChange: function(e){
     var that = this;
     var index = e.currentTarget.dataset.index;
     var condition = that.data.condition;
     if (index == condition){
+      wx.request({
+        method: 'POST',
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: app.globalData.webSite + '/Home/Wechat/collectionSelectByPhone',
+        data: {
+          user_phone: '18787312252',
+        },
+        success: function (res) {
+          var usedCar = [];
+          var newCar = [];
+          res.data.data.forEach(function (val, key) {
+            if (val.category == '2') {
+              usedCar.push(val);
+            } else if (val.category == '1') {
+              newCar.push(val)
+            }
+          });
+          that.setData({
+            newCar: newCar,
+            usedCar: usedCar
+          });
+        }
+      });
       that.setData({
         active: 'active',
         bgColor: 'bgColor',
@@ -21,6 +47,31 @@ Page({
         active1: ''
       })
     }else{
+      wx.request({
+        method: 'POST',
+        header: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: app.globalData.webSite + '/Home/Wechat/collectionSelectByPhone',
+        data: {
+          user_phone: '18787312252',
+        },
+        success: function (res) {
+          var usedCar = [];
+          var newCar = [];
+          res.data.data.forEach(function (val, key) {
+            if (val.category == '2') {
+              usedCar.push(val);
+            } else if (val.category == '1') {
+              newCar.push(val)
+            }
+          });
+          that.setData({
+            newCar: newCar,
+            usedCar: usedCar
+          });
+        }
+      });
       that.setData({
         active: 'active1',
         bgColor: '',
@@ -49,7 +100,7 @@ Page({
         res.data.data.forEach(function(val,key){
           if(val.category == '2'){
             usedCar.push(val);
-          }else{
+          } else if (val.category == '1'){
             newCar.push(val)
           }
         });
@@ -60,7 +111,116 @@ Page({
       }
     });
   },
-
+  //点击删除二手车收藏
+  deleteUsedCar: function(e){
+    var that = this;
+    var car_id = e.currentTarget.dataset.id;
+    wx.request({
+      method: 'POST',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      url: app.globalData.webSite + '/Home/Wechat/collectionDelete',
+      data: {
+        user_phone: '18787312252',
+        car_id: car_id
+      },
+      success: function (res) {
+        that.data.usedCar.forEach(function (val, key) {
+          console.log(val);
+          that.data.usedCar[key].state = '0';
+        });
+        that.data.usedCar.forEach(function (val, key) {
+          if (val.id == car_id) {
+            that.data.usedCar[key].state = '1';
+          }
+        });
+        that.setData({
+          usedCar: that.data.usedCar
+        });
+        wx.request({
+          method: 'POST',
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          url: app.globalData.webSite + '/Home/Wechat/collectionSelectByPhone',
+          data: {
+            user_phone: '18787312252',
+          },
+          success: function (res) {
+            var usedCar = [];
+            var newCar = [];
+            res.data.data.forEach(function (val, key) {
+              if (val.category == '2') {
+                usedCar.push(val);
+              } else if (val.category == '1') {
+                newCar.push(val)
+              }
+            });
+            that.setData({
+              newCar: newCar,
+              usedCar: usedCar
+            });
+          }
+        });
+      }
+    })
+  },
+  //点击删除新车收藏
+  deleteNewCar: function (e) {
+    var that = this;
+    var car_id = e.currentTarget.dataset.id;
+    wx.request({
+      method: 'POST',
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      url: app.globalData.webSite + '/Home/Wechat/collectionDelete',
+      data: {
+        user_phone: '18787312252',
+        car_id: car_id
+      },
+      success: function (res) {
+        // that.data.newCar.forEach(function (val, key) {
+        //   console.log(val);
+        //   that.data.newCar[key].state = '0';
+        // });
+        that.data.newCar.forEach(function (val, key) {
+          if (val.id == car_id) {
+            that.data.newCar[key].state = '1';
+          }
+        });
+        that.setData({
+          newCar: that.data.newCar
+        });
+        wx.request({
+          method: 'POST',
+          header: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          url: app.globalData.webSite + '/Home/Wechat/collectionSelectByPhone',
+          data: {
+            user_phone: '18787312252',
+          },
+          success: function (res) {
+            var usedCar = [];
+            var newCar = [];
+            res.data.data.forEach(function (val, key) {
+              if (val.category == '2') {
+                usedCar.push(val);
+              } else if (val.category == '1') {
+                newCar.push(val)
+              }
+            });
+            that.setData({
+              newCar: newCar,
+              usedCar: usedCar
+            });
+          }
+        });
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
